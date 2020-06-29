2802e45b43262243f1c6ffca14f0524e2f502b2d
Samuel Grayson <sam@samgrayson.me>
2020-06-29 14:47:31 -0500

    Fix warning

--- src/audio_component.cpp
+++ src/audio_component.cpp
@@ -21,11 +21,11 @@ public:
 	audio_component(std::string name_, phonebook *pb_)
 		: threadloop{name_, pb_}
 		, sb{pb->lookup_impl<switchboard>()}
-		, logger{"audio"}
 		, _m_pose{sb->subscribe_latest<pose_type>("slow_pose")}
 		, decoder{"", ILLIXR_AUDIO::ABAudio::ProcessType::DECODE}
 		, encoder{"", ILLIXR_AUDIO::ABAudio::ProcessType::ENCODE}
 		, last_iteration{std::chrono::high_resolution_clock::now()}
+		, logger{"audio"}
 	{
 		decoder.loadSource();
 		encoder.loadSource();
@@ -47,11 +47,10 @@ public:
 
 private:
 	const std::shared_ptr<switchboard> sb;
-	start_end_logger logger;
 	std::unique_ptr<reader_latest<pose_type>> _m_pose;
 	ILLIXR_AUDIO::ABAudio decoder, encoder;
-	std::chrono::time_point<std::chrono::system_clock> previous_time;
 	std::chrono::high_resolution_clock::time_point last_iteration;
+	start_end_logger logger;
 };
 
 PLUGIN_MAIN(audio_component)
